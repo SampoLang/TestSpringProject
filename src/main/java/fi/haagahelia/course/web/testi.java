bc0ae8eaffc8ec270afa36f71c870d1a6f1dd782
Sampo Lång <sampo.lang@gmail.com>
2022-09-07 13:11:45 +0300

    Koko projektin vieminen repositoryyn

---
+++ src/main/java/fi/haagahelia/course/web/testi.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.course.web;
+import java.util.Scanner;
+public class testi {
+
+	public static void main(String[] args) {
+		Scanner lukija = new Scanner(System.in);
+		System.out.println("Anna nimi: ");
+		String nimi = lukija.next();
+		System.out.println("Nimesi: " + nimi);
+
+	}
+
+}
